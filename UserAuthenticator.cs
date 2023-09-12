2fd6511323d6209b7c895ab04b44ebbb7c7a347d
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-12 20:09:09 +0530

    P3-Assignment2

---
+++ UserAuthenticator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppNUnitAssignment
+{
+    public class UserAuthenticator
+    {
+        private List<User> users = new List<User>();
+
+        public void RegisterUser(string username, string password)
+        {
+            // Simulate user registration by adding a user to the list
+            users.Add(new User { Username = username, Password = password });
+        }
+
+        public bool Login(string username, string password)
+        {
+            // Simulate user login by checking if the provided credentials match any user in the list
+            return users.Exists(u => u.Username == username && u.Password == password);
+        }
+
+        public void ResetPassword(string username, string newPassword)
+        {
+            // Simulate password reset by updating the user's password
+            var user = users.Find(u => u.Username == username);
+            if (user != null)
+            {
+                user.Password = newPassword;
+            }
+        }
+    }
+}
---
+++ UserAuthenticatorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppNUnitAssignment
+{
+    [TestFixture]
+    public class UserAuthenticatorTests
+    {
+        private UserAuthenticator userAuthenticator;
+
+        [SetUp]
+        public void Setup()
+        {
+            userAuthenticator = new UserAuthenticator();
+        }
+
+        [Test]
+        public void TestUserRegistration()
+        {
+            userAuthenticator.RegisterUser("Maximus", "Perseus");
+            // Assert that the user is registered
+            Assert.IsTrue(userAuthenticator.Login("Maximus", "Perseus"));
+        }
+
+        [Test]
+        public void TestUserLogin()
+        {
+            // Register a user first
+            userAuthenticator.RegisterUser("Maximus", "Perseus");
+
+            // Test user login
+            Assert.IsTrue(userAuthenticator.Login("Maximus", "Perseus"));
+            Assert.IsFalse(userAuthenticator.Login("Maxu", "Persu"));
+            Assert.IsFalse(userAuthenticator.Login("Persu", "Maxu"));
+        }
+
+        [Test]
+        public void TestPasswordReset()
+        {
+            // Register a user first
+            userAuthenticator.RegisterUser("Maximus", "Perseus");
+
+            // Reset the password for user "Maximus" to "Maximus"
+            userAuthenticator.ResetPassword("Maximus", "Maximus");
+
+            // Attempt login with the new password
+            Assert.IsTrue(userAuthenticator.Login("Maximus", "Maximus"));
+        }
+    }
+}
